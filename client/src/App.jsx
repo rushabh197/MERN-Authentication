534359ec031b25ddc78a941c1cf090d1e54fe964
rushabh197 <rushabhkumat563@gmail.com>
2025-09-29 00:17:20 +0530

    file added

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {" "}
+        </Route>
+        <Route path="/about" element={<About />}>
+          {" "}
+        </Route>
+        <Route path="/sign-in" element={<SignIn />}>
+          {" "}
+        </Route>
+        <Route path="/sign-up" element={<SignUp />}>
+          {" "}
+        </Route>
+        <Route path="/profile" element={<Profile />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
